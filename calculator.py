e69bf93c60786dc6e45a9ae464d34883c8bfc99c
Ankur Patel <39783407+patel0ankur@users.noreply.github.com>
2020-08-27 01:33:56 -0500

    update

--- calculator.py
+++ calculator.py
@@ -1,4 +1,5 @@
-
+first_term = 2
+second_term = 3
 
 def add(first_term, second_term):
     return first_term + second_term
@@ -10,3 +11,8 @@ def subtract(first_term, second_term):
 
 def multiply(first_term, second_term):
     return first_term * second_term
+
+
+print(add(first_term, second_term))
+print(subtract(first_term, second_term))
+print(multiply(first_term, second_term))
